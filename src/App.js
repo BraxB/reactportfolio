c9b551eefa8603f61bad5be78ceeec26c24d748a
BraxB <braxb92@gmail.com>
2021-07-07 22:26:48 -0400

    using react router instead of the weird workaround I was using. Began working on styling. Text is coming in wrong color

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer"
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import Work from './components/pages/Work';
+import Err from './components/pages/Err';
+import Navbar from './components/Navbar';
+// import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <PortfolioContainer/>
-    </div>
+      <Router>
+            <Switch>
+              <Route exact path="/">
+                <About />
+              </Route>
+              <Route exact path="/work">
+                <Work />
+              </Route>
+              <Route exact path="/contact">
+                <Contact />
+              </Route>
+              <Route component={Err} />
+            </Switch>
+      </Router>
   );
 }
 
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,7 +6,15 @@ export default function Work() {
             <div className="col-lg-6 gx-4">
                 <div className="container" id="about">
                     <h2>About</h2>
+                    <p>
+                        Hello, my name is Braxton. I am a developer from Atlanta, GA. I currently have experience working in a variety of technologies and frameworks in the MERN Stack. I am particularly fond of front-end development given my previous work in graphic design, but also enjoy the challenge of server-side development.
+                    </p>
                 </div>
+            </div>
+            <div className="col-lg-6" id="brax">
+                <figure>
+                    <img src="Assets/brax.png"></img>
+                </figure>
             </div>
         </div>
     )
