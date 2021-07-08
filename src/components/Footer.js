de3514d4870083e1412baf2ba9f4bf346c395a33
BraxB <braxb92@gmail.com>
2021-07-07 23:26:39 -0400

    fixed styling issue with footer not sticking and stopped y axis overflow on about page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,8 @@ import React from 'react';
 
 export default function Footer() {
     return (
-        <footer className="bg-dark text-center text-lg-start" id="contact">
-            <div className="row">
-                <div className="col-12">
-                    <h2>wow!</h2>
-                </div>
-            </div>
+        <footer className="bg-dark text-center text-lg-start footer">
+            <h2>some footer content</h2>
         </footer>
     )
 }
